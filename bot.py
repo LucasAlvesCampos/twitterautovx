acf602a0eeeec46b2219c9c5a4bb8af184bbbfe8
Lucas Alves Campos <lucasalvescampos@gmail.com>
2022-12-03 15:31:34 -0300

    ok

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
+from http.server import HTTPServer
+import os
 import telebot
+
+# Get port number from the PORT environment varaible or 3000 if not specified
+port = os.getenv('PORT', 3000)
+
+server = HTTPServer(('0.0.0.0', port), MyServer)
+server.serve_forever()
 
 CHAVE_API = "5900906903:AAGa8HMd6lRqXQftr50HKBvjoRkfMjB2zfE"
 
